33c158e43ae017a19d5021db530b7c098a3f62a9
Geoff <gm@sky.garden>
2019-06-13 11:51:42 +0300

    Updated settings for firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,13 @@
 
 export const environment = {
   production: false,
-  firebaseConfig: { apiKey: '', authDomain: '', databaseURL: '', projectId: '', storageBucket: '', messagingSenderId: '', appId: '', }
+  firebaseConfig: {
+    apiKey: 'AIza....',                             // Auth / General Use
+    authDomain: 'YOUR_APP.firebaseapp.com',         // Auth with popup/redirect
+    databaseURL: 'https://YOUR_APP.firebaseio.com', // Realtime Database
+    storageBucket: 'YOUR_APP.appspot.com',          // Storage
+    messagingSenderId: '123456789'                  // Cloud Messaging
+  }
 };
 
 /*
