e15aada0faa7b254cf9cf7bcd3f78b9c60891cf2
geoff <mac@macs-MacBook-Pro.local>
2020-06-22 23:45:08 +0300

    Added deployment configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,13 @@
 export const environment = {
   production: true,
-  // firebaseConfig: {
-  //   apiKey: 'AIza....',                             // Auth / General Use
-  //   authDomain: 'YOUR_APP.firebaseapp.com',         // Auth with popup/redirect
-  //   databaseURL: 'https://YOUR_APP.firebaseio.com', // Realtime Database
-  //   storageBucket: 'YOUR_APP.appspot.com',          // Storage
-  //   messagingSenderId: '123456789'                  // Cloud Messaging
-  // },
-  // publicKey: ''
-
-  firebaseConfig: { apiKey: 'AIzaSyDUbTvoLFuNumJEO8fLbBumWH1fLU1ZGkk', authDomain: 'vazi-aa474.firebaseapp.com', databaseURL: 'https://vazi-aa474.firebaseio.com', projectId: 'vazi-aa474', storageBucket: '', messagingSenderId: '145787836789', appId: '1:145787836789:web:d88a78a662d8760f' }, publicKey: 'BPXym4I47xqX9Zy4cw6ZVtOYkOOPPtM_nJcAr2y9wwzZTeT-cG9L4wg-HGJ0Ck4X2siDFT8Qekn3H1p6Y3t2FG8'
-
+  firebaseConfig: {
+    apiKey: 'AIzaSyDUbTvoLFuNumJEO8fLbBumWH1fLU1ZGkk',
+    authDomain: 'vazi-aa474.firebaseapp.com',
+    databaseURL: 'https://vazi-aa474.firebaseio.com',
+    projectId: 'vazi-aa474',
+    storageBucket: '',
+    messagingSenderId: '145787836789',
+    appId: '1:145787836789:web:d88a78a662d8760f'
+  },
+  publicKey: 'BPXym4I47xqX9Zy4cw6ZVtOYkOOPPtM_nJcAr2y9wwzZTeT-cG9L4wg-HGJ0Ck4X2siDFT8Qekn3H1p6Y3t2FG8'
 };
